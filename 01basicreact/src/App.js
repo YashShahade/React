d5d976700377c6dcc944ca2d9eef25e7b69e6e2e
yash shahade <yashshahade1834@gmail.com>
2025-08-27 23:21:20 +0530

    add index file

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+      <h1>Clasic react app</h1>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube(){
+    return(
+        <h1>clasic Youtube app</h1>
+    )
+}
+export default Youtube;
